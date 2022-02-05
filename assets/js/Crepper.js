e4db82f92deeef5dff1491b6d14280ca73996c09
30266 <63724619+30266-Official@users.noreply.github.com>
2022-02-05 19:38:38 +0800

    Delete Crepper.js

--- assets/js/Crepper.js
+++
@@ -1,5 +0,0 @@
-function Crepper(){
-    document.getElementById("Crepper").innerHTML="帕是屑";
-    var html ="<input type=\"button\" value=\"30是屑吗？\" onclick=a30266()>"
-    document.getElementById("add").innerHTML=html;
-}
