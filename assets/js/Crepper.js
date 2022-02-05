ee7774831b42f4a4ff891212e96e720b27c06c2a
30266 <63724619+30266-Official@users.noreply.github.com>
2022-02-05 19:36:11 +0800

    Update Crepper.js

--- assets/js/Crepper.js
+++ assets/js/Crepper.js
@@ -1,5 +1,5 @@
 function Crepper(){
     document.getElementById("Crepper").innerHTML="帕是屑";
-    var html ="<input type=\"button\" value=\"30是屑吗？\" οnclick=a30266()>"
+    var html ="<input type=\"button\" value=\"30是屑吗？\" nclick=a30266()>"
     document.getElementById("add").innerHTML=html;
 }
